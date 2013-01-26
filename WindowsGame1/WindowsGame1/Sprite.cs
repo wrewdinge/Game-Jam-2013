f88f6852de455b5358e66a57041f5a89ee20faf6
wrewdinge <wrewdinge@gmail.com>
2013-01-26 11:31:44 -0800

    Added Sprite.cs and Unit.cs

---
+++ WindowsGame1/WindowsGame1/Sprite.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+	class Sprite
+	{
+		private Texture2D mSpriteSheet;
+		private Vector2 mLocation;
+		private float mRotation;
+		private Rectangle mSheetLocation;
+		private Color mColor;
+		private Vector2 mOrigin;
+		private SpriteEffects mSpriteEffect;
+		private float mLayer;
+		private float mScale;
+
+		public Sprite()
+		{
+			mSpriteSheet = null;
+			mLocation = Vector2.Zero;
+			mRotation = 0;
+			mSheetLocation = Rectangle.Empty;
+			mColor = Color.White;
+			mOrigin = Vector2.Zero;
+			mSpriteEffect = SpriteEffects.None;
+			mLayer = 0;
+		}
+
+		public Sprite(Texture2D spriteSheet, Vector2 location, Rectangle sheetLocation, Color color, Vector2 origin, float scale = 1, float rotation = 0, SpriteEffects spriteEffect = SpriteEffects.None, float layer = 0)
+		{
+			mSpriteSheet = spriteSheet;
+			mLocation = location;
+			mSheetLocation = sheetLocation;
+			mColor = color;
+			mOrigin = origin;
+			mSpriteEffect = spriteEffect;
+			mLayer = layer;
+			mScale = scale;
+		}
+
+		public void draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(mSpriteSheet, mLocation, mSheetLocation, mColor, mRotation, mOrigin, mScale, mSpriteEffect, mLayer);
+		}
+	}
+}
