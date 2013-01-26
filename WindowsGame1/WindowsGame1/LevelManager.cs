85eb5c08fc882a58f9e6765c3093bcc18b51a512
wrewdinge <wrewdinge@gmail.com>
2013-01-26 12:13:26 -0800

    Added LevelManager.cs

---
+++ WindowsGame1/WindowsGame1/LevelManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+	public enum CurrentLevel
+	{
+		ChildHood = 0,
+		Teenager,
+		Twenties,
+		MiddleAge,
+		OldAge
+	}
+
+	class LevelManager
+	{
+		private List<Rectangle>	mCollideables;
+		private List<Sprite>	mSprites;
+		private Rectangle		mGoal;
+		private List<Unit>		mUnits;
+		public CurrentLevel eCurrLevel;
+
+		public LevelManager()
+		{
+			mCollideables = new List<Rectangle>();
+			mSprites = new List<Sprite>();
+			eCurrLevel = CurrentLevel.ChildHood;
+			mGoal = Rectangle.Empty;
+		}
+
+		public void addObjects()
+		{
+			switch (eCurrLevel)
+			{
+				case CurrentLevel.ChildHood:
+
+					break;
+
+				case CurrentLevel.Teenager:
+
+					break;
+
+				case CurrentLevel.Twenties:
+
+					break;
+					
+				case CurrentLevel.MiddleAge:
+
+					break;
+
+				case CurrentLevel.OldAge:
+
+					break;
+			}
+		}
+	}
+}
